ea488674c0e9257c8b2579bc57912fe963ade0a3
Alessys <Alessys@ALESYA>
2023-04-26 10:57:54 +0300

    добавила класс ShellSort

--- Laba 10 OAIP/ComparativeAnalysis.Designer.cs
+++ Laba 10 OAIP/ComparativeAnalysis.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ComparativeAnalysis";
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(4, 5);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(300, 188);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 215);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 250);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 2;
+            label2.Text = "label2";
+            // 
+            // ComparativeAnalysis
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "ComparativeAnalysis";
+            Text = "ComparativeAnalysis";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
     }
 }
--- Laba 10 OAIP/ComparativeAnalysis.cs
+++ Laba 10 OAIP/ComparativeAnalysis.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,85 @@ namespace Laba_10_OAIP
 {
     public partial class ComparativeAnalysis : Form
     {
+        private Random random = new Random();
+        public static int Comparison = 0;
+        public static int NumberOfPermutations = 0;
+        public static string elapsedTime = "";
+        public static int timeSort = 0;
+        public static List<SortingResultsInformation> sortingResults = new
+       List<SortingResultsInformation>();
+        private Context context = new Context();
+
         public ComparativeAnalysis()
         {
             InitializeComponent();
+            dataGridView1.Columns.Add("Volume", "Объем выборки");
+            dataGridView1.Columns.Add("BubbleSort", "Метод обмена");
+            dataGridView1.Columns.Add("ShellSort", "Метод Шелла");
+            25
+ dataGridView1.Rows.Add("10");
+            dataGridView1.Rows.Add("100");
+            dataGridView1.Rows.Add("1000");
+            dataGridView1.Rows.Add("10000");
+            dataGridView1.Rows.Add("100000");
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].ReadOnly = true;
+            }
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void FillArray(int[] vs)
+        {
+            for (int i = 0; i < vs.Length; i++)
+            {
+                vs[i] = random.Next(0, 100000);
+            }
+        }
+
+        private void Sort(int n, int number)
+        {
+            this.context = new Context(new BubbleSort());
+            Context.array = new int[n];
+            FillArray(Context.array);
+            context.ExecuteAlgorithm(false);
+            dataGridView1.Rows[number].Cells[1].Value += "С: " +
+           Convert.ToString(Comparison) + " ";
+            dataGridView1.Rows[number].Cells[1].Value += "П: " +
+           Convert.ToString(NumberOfPermutations) + " ";
+            dataGridView1.Rows[number].Cells[1].Value += "t: " +
+           Convert.ToString(elapsedTime);
+            sortingResults.Add(new SortingResultsInformation(Comparison,
+           NumberOfPermutations, elapsedTime, new BubbleSort(), timeSort, n));
+            Comparison = 0;
+            NumberOfPermutations = 0;
+            timeSort = 0;
+            elapsedTime = "";
+            Context.array = null;
+            this.context = new Context(new ShellSort());
+            Context.array = new int[n];
+            FillArray(Context.array);
+ context.ExecuteAlgorithm(false);
+            dataGridView1.Rows[number].Cells[2].Value += "С: " +
+           Convert.ToString(Comparison) + " ";
+            dataGridView1.Rows[number].Cells[2].Value += "П: " +
+           Convert.ToString(NumberOfPermutations) + " ";
+            dataGridView1.Rows[number].Cells[2].Value += "t: " +
+           Convert.ToString(elapsedTime);
+            sortingResults.Add(new SortingResultsInformation(Comparison,
+           NumberOfPermutations, elapsedTime, new ShellSort(), timeSort, n));
+            Comparison = 0;
+            NumberOfPermutations = 0;
+            timeSort = 0;
+            elapsedTime = "";
+            Context.array = null;
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
